eec287c16fc57f17768b53aa505ff02d8f875c6c
Mikhail Chaus <setting.chaus@gmail.com>
2022-09-23 12:36:16 +0200

    Set navBar

--- blur-task/Resources /Colors.swift
+++ blur-task/Resources /Colors.swift
@@ -2,6 +2,7 @@ import UIKit
 
 extension UIColor {
     static let background = UIColor(hex: 0x141822)
-    static let title = UIColor(hex: 0xFFFFFF)
+    static let secondary = UIColor(hex: 0x343737)
     static let details = UIColor(hex: 0xE3E34E)
+    static let title = UIColor(hex: 0xFFFFFF)
 }
--- blur-task/Sources/Modules/Filter/FilterViewController.swift
+++ blur-task/Sources/Modules/Filter/FilterViewController.swift
@@ -4,5 +4,39 @@ final class FilterViewController: BaseViewController<FilterView> {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setup()
+    }
+}
+
+// MARK: - Setup
+
+private extension FilterViewController {
+    
+    func setup() {
+        setupNavigation()
+    }
+    
+    func setupNavigation() {
+        guard let navigationBar = navigationController?.navigationBar else { return }
+        
+        lazy var leftLabel = UILabel().do {
+            $0.text = "Filters"
+            $0.textColor = .title
+            $0.font = .boldSystemFont(ofSize: 35)
+            $0.isEnabled = false
+        }
+        
+        lazy var rightButton = UIButton().do {
+            let height: CGFloat = 30
+            $0.frame = .init(x: 0, y: 0, width: height, height: height)
+            $0.layer.cornerRadius = height / 2
+            $0.backgroundColor = .secondary
+            $0.tintColor = .details
+            $0.setImage(UIImage(systemName: "gearshape.fill"), for: .normal)
+        }
+        
+        navigationBar.isTranslucent = true
+        navigationItem.leftBarButtonItem = .init(customView: leftLabel)
+        navigationItem.rightBarButtonItem = .init(customView: rightButton)
     }
 }
