a2f9744bacdd9fa49a343963c9eda3dcbc82abc3
Mikhail Chaus <setting.chaus@gmail.com>
2022-09-28 16:50:08 +0200

    minor ref

--- Task/Sources/Application/SceneDelegate.swift
+++ Task/Sources/Application/SceneDelegate.swift
@@ -3,15 +3,15 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     var window: UIWindow?
-    var navigationController: UINavigationController?
+    private(set) var navigationController: UINavigationController?
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let windowScene = (scene as? UIWindowScene) else { return }
+        guard let scene = (scene as? UIWindowScene) else { return }
         
         navigationController = .translucentNavigationController()
         navigationController?.pushViewController(PhotoStreamViewController(), animated: false)
         
-        window = UIWindow(windowScene: windowScene)
+        window = UIWindow(windowScene: scene)
         window?.rootViewController = navigationController
         window?.backgroundColor = .background
         window?.makeKeyAndVisible()
